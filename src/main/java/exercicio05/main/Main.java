6a483cd05c13abf8b02b4b9a7bc07bb3c2c5b8b8
Ronan <ronan-com@hotmil.com>
2021-03-09 21:05:06 -0300

    Primeiro commit

---
+++ src/main/java/exercicio05/main/Main.java
@@ -0,0 +1,45 @@
+package exercicio05.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		
+		for (int i =0; i <= 5; i++) {
+			
+			switch(i) {
+			
+			case 1:
+				System.out.println("Primeira condição");
+			
+				break;
+			
+			case 4:
+				System.out.println("Segunda condição");
+				
+				break;
+				
+			default:
+				System.out.println("Não encontrou uma opção");
+				
+				break;
+			}
+			
+				
+			
+		}
+		
+		
+		
+		
+	}
+
+}
